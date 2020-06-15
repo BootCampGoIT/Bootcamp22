15d494791a026e3af760358261e696f56ffa6a79
AndriiShnurkov <Shans12.84@ukr.net>
2020-06-15 19:23:19 +0300

    added module 1 lesson 1

---
+++ Module 1/Lesson 1/index.js
@@ -0,0 +1,224 @@
+// =============== let ==============
+
+// let x;
+
+// x = 5;
+// x = `${5}`
+
+
+// =============== const ==============
+
+// const x = 5;
+
+
+// =============== Number ==============
+
+// const x = 0.1;
+// const y = 0.2;
+// const num = (x * 10 + y * 10) / 10;
+// console.log(num);
+
+
+// =============== String ==============
+
+// const string = "Hello";
+// const person = "Alex";
+// // const result = string + ' ' + person + '!'
+// const result = `${string} ${person}!`;
+// console.log(result);
+
+// console.log(`${string} ${person}!`);
+
+
+// const name = "Notebook";
+// let quantity = 3;
+
+// console.log(`${name}: ${quantity}`);
+
+// quantity += 1; // quantity = quantity + 1;
+// // quantity++
+// console.log(`${name}: ${quantity}`);
+
+
+// =============== Boolean ==============
+
+// let isLogin = true; //true
+// let isAuthenticated = true;
+// let token = '';
+
+// // falsy:   undefinded, 0, NaN, '', null, false 
+
+// // console.log(isLogin);//true
+
+// if (isLogin && isAuthenticated && token) {
+//     console.log("Hello Nikita!");
+// }
+
+
+// =============== null ==============
+
+// let x = null;
+
+// =============== undefined ==============
+
+// let x;
+// console.log(x);
+// x = "Hello"
+// console.log(x)
+
+// console.log(x);
+
+
+// =============== typeof ==============
+
+// console.dir(window)
+// let a = 5;
+
+// switch (typeof a) {
+//     case 'string':
+//         console.log("Your data is String");
+//         break;
+//     case 'number':
+//         console.log("Your data is Number");
+//         break;
+//     default:
+//         break;
+// }
+
+// if ((typeof a) === 'string') {
+//     console.log("Your data is String");
+// }
+// if ((typeof a) === 'number') {
+//     console.log("Your data is Number");
+// }
+
+
+
+
+// -----------------------------------------------------
+
+// =============== console ==============
+// console.log();
+// console.log(document)
+// console.dir(document)
+
+// =============== alert ==============
+// let a = 5;
+// let b = 10;
+
+// alert(`Sum: ${a + b}`);
+// console.log("Hello")
+
+// =============== prompt ==============
+// const productName = "Milk";
+// let quantity = 10;
+
+// let input = prompt('Enter your quantity!');
+
+// const isInput = isNaN(input); 
+
+// if ((input !== null) && !isInput)  {
+//     quantity = Number(input)
+// }
+// console.log(`${productName}: ${quantity}`);
+
+
+// isNaN
+
+// let input = prompt("Enter number"); 
+// console.log(isNaN(true)); // 4 - false // dsfsdgdfdf - true
+
+
+
+
+
+
+
+// =============== confirm ==============
+
+// // falsy:   undefinded, 0, NaN, '', null, false 
+
+// const input = confirm("Is you agree with terms");
+// console.log(input);
+
+// if (input === true) {
+//     console.log("Hello")
+// }
+// if (input) {
+//     console.log("Hello")
+// }
+
+
+// if (input === false) {
+//     console.log("By")
+// }
+// if (!input) {
+//     console.log("By")
+// }
+
+
+
+
+// =============== Math ==============
+
+
+
+// const message = 'JavaScript is awesome!';
+
+// // console.log(message);
+
+// const name = 'Mango';
+
+// // console.log('My name is: ', name);
+
+
+// alert(message);
+
+
+// let a = 10;
+// let b = 10;
+
+// if (a > b) {
+//     console.log(`a > b`)
+// } 
+// if ( a < b) {
+//     console.log(`a < b`)
+// }
+// if ( a !== b) {
+//     console.log(`a = b`)
+// }
+
+
+
+let a = 5;
+const b = 10;
+if (a > b) {
+
+
+}
+
+
+
+
+
+
+// // falsy:   undefinded, 0, NaN, '', null, false 
+
+
+
+// const a = 5;
+// const b = '5'
+// console.log(a !== b);
+
+
+// ============== hw-1-1 ==============
+
+// const a = 5;
+// const b = '5'
+// console.log(a !== b);
+// ============== hw-1-2 ==============
+
+// const a = 5;
+// const b = '5'
+// console.log(a !== b);
+
